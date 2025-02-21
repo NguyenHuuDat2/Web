9051c667f618a622578b0999717a35df37bc3ae0
NguyenHuuDat2 <huudat2694tp@gmail.com>
2025-02-21 23:44:40 +0700

    upload file khtn lan1

---
+++ Moduele2/script.js
@@ -0,0 +1,7 @@
+function bat_den() {
+    document.getElementById('bulb').src = '/images/pic_bulbon.gif';
+}
+
+function tat_den() {
+    document.getElementById('bulb').src = '/images/pic_bulboff.gif';
+}
